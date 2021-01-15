05c58c801e0f7030a79cbda76f15fccbae5a6122
cooperhurt <40371983+cooperhurt@users.noreply.github.com>
2021-01-14 19:52:00 -0700

    Setup Schema modification

--- src/models/setups.ts
+++ src/models/setups.ts
@@ -6,23 +6,24 @@ const Schema = mongoose.Schema;
 
 const SetupSchema = new Schema({
     Active: { type: Boolean, required: true },
-    Name: { type: String, required: true },
-    DriverName: { type: String, required: false },
-    ImagePath: {
-        type: String,
-        required: false,
-        default: "http://setuprc.com/img/SetupRc/defaultTrackCover.png",
-    },
-    SetupPath: { type: String, required: false },
-    EventName: { type: String, required: false },
-    Surface: { type: String, required: false },
-    Size: { type: String, required: false },
     Car: {
         type: Schema.Types.ObjectId,
         ref: "Car",
         autopopulate: true,
         required: true,
     },
+    DriverName: { type: String, required: false },
+    EventName: { type: String, required: false },
+    ImagePath: {
+        type: String,
+        required: false,
+        default: "http://setuprc.com/img/SetupRc/defaultTrackCover.png",
+    },
+    Name: { type: String, required: true },
+    SetupPath: { type: String, required: false },
+    Size: { type: String, required: false },
+    Surface: { type: String, required: false },
+    Tips: { type: [String], required: false },
     TrackCondition: { type: String, required: false },
 });
 
